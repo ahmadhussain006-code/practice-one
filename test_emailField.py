36cebc5625f9af552bcc7ad5cf8cad334a0afde1
Ahmad Hussain <ahmad.hussain006@gmail.com>
2026-03-04 17:59:15 +0530

    replace false to true

--- test_emailField.py
+++ test_emailField.py
@@ -1,6 +1,6 @@
 from playwright.sync_api import sync_playwright
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
+    browser = p.chromium.launch(headless=True)
     page = browser.new_page()
     page.goto('https://demo.automationtesting.in/Index.html')
     # used - #id
--- test_launch_demoPage.py
+++ test_launch_demoPage.py
@@ -1,7 +1,7 @@
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
+    browser = p.chromium.launch(headless=True)
     page = browser.new_page()
     page.goto('https://demo.automationtesting.in/Index.html')
     page.wait_for_timeout(3000)
